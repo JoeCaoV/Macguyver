1fc1ec158a988d587acd60ea64ccdcf6091ea3fa
Joevin Caovan <joe.caovan@gmail.com>
2019-02-13 01:57:36 +0100

    index rename to main, config file added

---
+++ config.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+TILE_SIZE = 40
+IMG_PATH = 'ressource/image/'
+FLOOR = pygame.image.load('{}floor.jpg'.format(IMG_PATH))
+WALL = pygame.image.load('{}wall.jpg'.format(IMG_PATH))
+BOT_LEFT = pygame.Rect(15, 615, 250, 50)
+BOT_RIGHT = pygame.Rect(450, 615, 250, 50)
+FONT = pygame.font.Font(None, 20)
+BIG_FONT = pygame.font.Font(None, 40)
